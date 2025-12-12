77885a76b4b4720d94505aae9d6943ef02bff305
MarckyDev <MarckyDev@users.noreply.github.com>
2025-12-12 08:04:57 +0000

    deploy: 82013c0b876144bdb30a91cdcb1141280a35a53e

--- _next/static/_vsPhBS-QRrlVzWMVj5z1/_buildManifest.js
+++ _next/static/_vsPhBS-QRrlVzWMVj5z1/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/purple-logbook//_next/:path+",
+        "destination": "/purple-logbook/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
